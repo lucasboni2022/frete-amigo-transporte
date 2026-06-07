f715548cf30d5e61df0f561cdbd6bbdc2b5359ee
lucasboni2022 <148803156+lucasboni2022@users.noreply.github.com>
2026-06-07 18:23:15 -0300

    add server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import { dirname, join } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-   
+    
 const app = express();
 const PORT = process.env.PORT || 3000;
 
